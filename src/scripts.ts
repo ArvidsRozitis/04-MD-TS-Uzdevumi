a5719f174615015133138b42f747413abd7ca36a
Arvids Rozitis <arvids.rozitis.cits@gmail.com>
2023-01-08 02:19:31 +0200

    done some more tasks

--- src/scripts.ts
+++ src/scripts.ts
@@ -707,6 +707,249 @@ For example, the array ['Alf', 'Alice', 'Ben'] should be transformed to
 { a: ['Alf', 'Alice'], b: ['Ben']}
 */
 
+const firstLetterObj = (arr: any[]) => {
+    let firstLetterObj: any = {};
+    for( let i = 0; i < arr.length; i++) {
+        let firstChar = arr[i][0];
+        if(!firstLetterObj[firstChar]) {
+            firstLetterObj[firstChar] = [];         
+        }
+        firstLetterObj[firstChar].push(arr[i]);
+    }
+    return firstLetterObj;        
+}
+
+console.log('///Task 42 with openAI help');
+console.log(firstLetterObj(['Alf', 'Alice', 'Ben']));//{ a: ['Alf', 'Alice'], b: ['Ben']}
+console.log(firstLetterObj(['Ant', 'Bear', 'Bird']));//{ a: ['Ant'], b: ['Bear', 'Bird']}
+console.log(firstLetterObj(['Berlin', 'Paris', 'Prague']));//{ b: ['Berlin'], p: ['Paris', 'Prague']}
+
+/*
+Task 43 (page 19)
+
+Write a function that takes an array with arbitrary elements and a number as arguments
+Return a new array, the first element should be either the given number itself
+or zero if the number is smaller than 6
+The other elements should be the elements of the original array
+Try not to mutate the original array
+*/
 
 
-console.log('///Task 42');
+const addElementIfLarger6NotMutateArr = (arr: any[], num: number): any[] => {
+    const numberArr = [...arr];
+    if(num >= 6) {
+        numberArr.unshift(num);
+    } else {
+        numberArr.unshift(0);
+    }
+    return numberArr; 
+}
+
+console.log('///Task 43');
+console.log(addElementIfLarger6NotMutateArr([1,2,3], 6)) //[6,1,2,3]
+console.log(addElementIfLarger6NotMutateArr(['a','b'], 2)) //[0,'a','b']
+console.log(addElementIfLarger6NotMutateArr([null,false], 11)) //[11,null,false]
+
+/*
+Task 44 (page 20)
+
+Write a function that takes an array (a) and a value (n) as arguments
+Save every nth element in a new array
+Return the new array
+*/
+
+const everyNValueToNewArr = (arr: number[], n: number): number[] => {
+    let newArr: number[] = [];
+    const everyN: number = n;
+    while( n <= arr.length) {
+        newArr.push(arr[n-1]);
+        n+=everyN;
+    }
+    return newArr;
+}
+
+console.log('///Task 44');
+console.log(everyNValueToNewArr([1,2,3,4,5,6,7,8,9,10],3)); // [3, 6, 9]
+console.log(everyNValueToNewArr([10,9,8,7,6,5,4,3,2,1],5)); // [6,1]
+console.log(everyNValueToNewArr([7,2,1,6,3,4,5,8,9,10],2)); // [2,6,4,8,10]
+
+/*
+Task 45 (page 20)
+
+Write a function that takes an object with two properties as argument
+It should return the value of the property with key country
+*/
+
+const objCountry = (object: {continent: string, country: string}) => object.country ; 
+
+console.log('///Task 45');
+console.log(objCountry({continent: 'Asia',  country: 'Japan'})); //'Japan'
+console.log(objCountry({  country: 'Sweden',  continent: 'Europe'})); //'Sweden'
+
+/*
+Task 46 (page 20)
+
+Write a function that takes an object with two properties as argument
+It should return the value of the property with key 'prop-2'
+Tip: you might want to use the square brackets property accessor
+*/
+
+const getProp2 = (obj: any): any => obj['prop-2'];
+
+
+console.log('///Task 46 šeit baigi nosprūdu, sapratu, ka kaut ko nesaprotu un ar AI palīdzību bedzot sapratu ko nesaprotu');
+console.log(getProp2({  one: 1,  'prop-2': 2})); // 2
+console.log(getProp2({'prop-2': 'two',  prop: 'test'})); // 'two'
+
+
+/*
+Task 47 (page 21)
+
+Write a function that takes an object with two properties and a string as arguments
+It should return the value of the property with key equal to the value of the string
+*/
+
+const getProperty = (obj: any, prop: string): any => obj[prop]; 
+
+console.log('///Task 47');
+console.log(getProperty({  continent: 'Asia',  country: 'Japan'}, 'continent')); //'Asia' 
+console.log(getProperty({  country: 'Sweden',  continent: 'Europe'}, 'country')); //'Sweden'
+
+/*
+Task 48 (page 21)
+
+Write a function that takes an object (a) and a string (b) as argument
+Return true if a has a property with key b
+Return false otherwise
+*/
+
+const thereIsPropertyB = (obj: any, b:string):boolean => b in obj;
+
+console.log('///Task 48');
+console.log(thereIsPropertyB({a:1,b:2,c:3},'b')); //true
+console.log(thereIsPropertyB({x:'a',y:'b',z:'c'},'a')); //false
+console.log(thereIsPropertyB({x:'a',y:'b',z:'c'},'z')); //true
+
+/*
+Task 49 (page 21)
+
+Write a function that a string (a) as argument
+Create an object that has a property with key 'key' and a value of a
+Return the object
+*/
+
+const argumentAsKeyValue = (str: string):any => {
+    return {key: str};
+}
+
+console.log('///Task 49');
+console.log(argumentAsKeyValue('a')); //{key:'a'}
+console.log(argumentAsKeyValue('z')); //{key:'z'}
+console.log(argumentAsKeyValue('b')); //{key:'b'}
+
+/*
+Task 50 (page 22)
+
+Write a function that takes two strings (a and b) as arguments
+Create an object that has a property with key 'a' and a value of 'b'
+Return the object
+*/
+
+const twoStringsToObj = (arg1:string, arg2:string) => {
+    return {[arg1]: arg2};
+};
+
+console.log('///Task 50');
+console.log(twoStringsToObj('a', 'b')); // {a:'b'}
+console.log(twoStringsToObj('z','x')); // {z:'x'}
+console.log(twoStringsToObj('b','w')); // {b:'w'}
+
+/*
+Task 51 (page 22)
+
+Write a function that takes two arrays (a and b) as arguments
+Create an object that has properties with keys 'a' and corresponding values 'b'
+Return the object
+*/
+
+const twoArrToObj = (keys: any[], values: any[]) => {
+    let itemObj: any = {}
+    keys.forEach((key, i) => itemObj[key] = values[i] )
+    return itemObj;
+}
+
+console.log('///Task 51');
+console.log(twoArrToObj(['a','b','c'],[1,2,3])); //{a:1,b:2,c:3}
+console.log(twoArrToObj(['w','x','y','z'],[10,9,5,2])); //{w:10,x:9,y:5,z:2}
+console.log(twoArrToObj([1,'b'],['a',2])); //{1:'a',b:2}
+
+/*
+Task 52 (page 22)
+
+Write a function that takes an object (a) as argument
+Return an array with all object keys
+*/
+
+const keysInObject = (obj: any) => Object.keys(obj);
+
+console.log('///Task 52');
+console.log(keysInObject({a:1,b:2,c:3})); //['a','b','c']
+console.log(keysInObject({j:9,i:2,x:3,z:4})); //['j','i','x','z']
+console.log(keysInObject({w:15,x:22,y:13})); //['w','x','y']
+
+/*
+Task 53 (page 23)
+
+Write a function that takes an object (a) as argument
+Return the sum of all object values
+*/
+
+const sumOfAllObjectValues = (obj: any) => {
+    const valueArr: number[] = Object.values(obj);
+    return valueArr.reduce((a, b) => a + b);
+}
+
+console.log('///Task 53');
+console.log(sumOfAllObjectValues({a:1,b:2,c:3})); // 6
+console.log(sumOfAllObjectValues({j:9,i:2,x:3,z:4})); // 18
+console.log(sumOfAllObjectValues({w:15,x:22,y:13})); // 50
+
+/*
+Task 54 (page 23)
+
+Write a function that takes an object as argument
+It should return an object with all original object properties
+except for the property with key 'b'
+*/
+
+const removeBFromObj = (obj: any) => {
+    delete obj['b'];
+    return obj;
+}
+console.log('///Task 54');
+console.log(removeBFromObj({ a: 1, b: 7, c: 3 })); //{ a: 1, c: 3 }
+console.log(removeBFromObj({ b: 0, a: 7, d: 8 })); //{ a: 7, d: 8 }
+console.log(removeBFromObj({ e: 6, f: 4, b: 5, a: 3 })); //{ e: 6, f: 4, a: 3 }
+
+/*
+Task 55 (page 23)
+
+Write a function that takes two objects as arguments
+Unfortunately, the property 'b' in the second object has the wrong key
+should be named 'd' instead
+Merge both objects and correct the wrong property name
+Return the resulting object
+It should have the properties 'a', 'b', 'c', 'd', and 'e'
+*/
+const mergeAndFix = (obj1: any, obj2: any): any => {
+    obj2['d'] = obj2['b']
+    delete obj2['b'];
+    const mergedObj = {...obj1, ...obj2};
+    return mergedObj;
+}
+
+console.log('///Task 55');
+console.log(mergeAndFix({ a: 1, b: 2 }, { c: 3, b: 4, e: 5 }))//{ a: 1, b: 2, c: 3, e: 5, d: 4}
+console.log(mergeAndFix({ a: 5, b: 4 }, { c: 3, b: 1, e: 2 }))//{ a: 5, b: 4, c: 3, e: 2, d: 1}
+
+
